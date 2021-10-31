2abde0581e637861db19a15e1258511aab9afb8c
arisaidalvarez <arisaidalvarez@gmail.com>
2021-10-31 11:43:23 -0600

    Java project created

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Rotating Plane
+
+Java program for a simple rotating plane
---
+++ src/mx/aam/SimpleRotatingPlane.java
@@ -0,0 +1,14 @@
+package mx.aam;
+
+import javax.swing.JFrame;
+
+public class SimpleRotatingPlane extends JFrame{
+	
+	public static void main(String[] args) {
+		
+		SimpleRotatingPlane frame = new SimpleRotatingPlane();
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+	}
+	
+}
